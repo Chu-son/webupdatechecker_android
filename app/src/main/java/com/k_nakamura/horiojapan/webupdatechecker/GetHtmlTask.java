1561baa78bebf305e18f324ee0f72cd3eb3bc4d1
KosukeNakamura <s15mh133@mail.saitama-u.ac.jp>
2016-08-18 00:15:59 +0900

    update

--- app/src/main/java/com/k_nakamura/horiojapan/webupdatechecker/GetHtmlTask.java
+++ app/src/main/java/com/k_nakamura/horiojapan/webupdatechecker/GetHtmlTask.java
@@ -1,6 +1,12 @@
 package com.k_nakamura.horiojapan.webupdatechecker;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Debug;
+import android.util.Log;
+import android.util.StringBuilderPrinter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +16,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.regex.Pattern;
 
 /**
  * Created by KOSUKE on 2016/08/14.
@@ -17,6 +26,25 @@ import java.net.URL;
 public class GetHtmlTask
         extends AsyncTask<URL, Void, String>
 {
+    private TextView tv_html;
+    private TextView tv_hash;
+    private Button btn_get;
+
+    private String preHtml;
+
+    SharedPreferences shPref;
+
+    public GetHtmlTask(TextView tv_html, TextView tv_hash, Button getButton, SharedPreferences shPref)
+    {
+        super();
+        this.tv_html = tv_html;
+        this.tv_hash = tv_hash;
+        this.btn_get = getButton;
+
+        this.shPref = shPref;
+        preHtml = shPref.getString("html","");
+    }
+
     @Override
     protected String doInBackground(URL... urls) {
         // 取得したテキストを格納する変数
@@ -37,7 +65,8 @@ public class GetHtmlTask
                 // 通信に成功した
                 // テキストを取得する
                 final InputStream in = con.getInputStream();
-                final String encoding = con.getContentEncoding();
+                String encoding = con.getContentEncoding();
+                if (encoding == null) encoding = "UTF8";
                 final InputStreamReader inReader = new InputStreamReader(in, encoding);
                 final BufferedReader bufReader = new BufferedReader(inReader);
                 String line = null;
@@ -64,4 +93,82 @@ public class GetHtmlTask
         }
         return result.toString();
     }
+
+    @Override
+    protected void onPostExecute(String result) {
+        String diff;
+        result = getCombinedStr(splitHtmlTags(result));
+        preHtml = getCombinedStr(splitHtmlTags(preHtml));
+
+        if(!result.equals(preHtml)) {
+            diff = getUpdatedLines(preHtml, result);
+        }
+        else diff = "";
+        tv_hash.setText(diff);
+        tv_html.setText(result);
+
+        SharedPreferences.Editor e = shPref.edit();
+        e.putString("html", result);
+        e.apply();
+
+        btn_get.setText("GET");
+        btn_get.setEnabled(true);
+    }
+
+    protected String getUpdatedLines(String str_A, String str_B) {
+        HashSet<String> hash_A = null;
+        HashSet<String> hash_B = null;
+
+        if (!str_A.isEmpty()) {
+            hash_A = getHashSet(str_A);
+        }
+
+        if (!str_B.isEmpty()) {
+            hash_B = getHashSet(str_B);
+        }
+
+        if (hash_A != null && hash_B != null) {
+            hash_A.removeAll(hash_B);
+        }
+
+        Iterator it = hash_A.iterator();
+        StringBuilder sb = new StringBuilder();
+        while(it.hasNext())
+        {
+            sb.append(it.next()+"\n");
+        }
+
+        return sb.toString();
+    }
+
+    protected HashSet<String> getHashSet(String str) {
+        if (str.isEmpty()) {
+            return null;
+        } else {
+            HashSet<String> hashSet = new HashSet<>();
+            String[] str_list = str.split("\n");
+            for(int i = 0; i < str_list.length;i++)
+            {
+                hashSet.add(str_list[i].trim());
+            }
+            return hashSet;
+        }
+    }
+
+    protected String[] splitHtmlTags(String str)
+    {
+        Pattern p = Pattern.compile("(/.*?)?>\\s*<|[<>]");
+        return p.split(str);
+    }
+    protected String getCombinedStr(String[] str_list)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str_list.length ; i++)
+        {
+            if(str_list[i].trim().isEmpty()) continue;
+            sb.append(str_list[i].trim()+"\n");
+        }
+        return sb.toString();
+    }
+
 }
--- app/src/main/java/com/k_nakamura/horiojapan/webupdatechecker/MainActivity.java
+++ app/src/main/java/com/k_nakamura/horiojapan/webupdatechecker/MainActivity.java
@@ -1,17 +1,21 @@
 package com.k_nakamura.horiojapan.webupdatechecker;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
     TextView tv_html;
     TextView tv_hash;
     Button btn_gethtml;
 
-    String html_str;
+    SharedPreferences shPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +26,25 @@ public class MainActivity extends AppCompatActivity {
         tv_html = (TextView)findViewById(R.id.html);
         btn_gethtml = (Button)findViewById(R.id.get_button);
 
+        shPref = getSharedPreferences("shPref",MODE_PRIVATE);
+        tv_html.setText(shPref.getString("html",""));
+
         btn_gethtml.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        btn_gethtml.setText("...");
+                        btn_gethtml.setEnabled(false);
+                        try {
+                            new GetHtmlTask(tv_html,tv_hash,btn_gethtml,shPref)
+                                    .execute(new URL("http://www.keyakizaka46.com/"));
+                        }catch (MalformedURLException e){
+                            e.printStackTrace();
+                        }
                     }
                 }
         );
     }
-
 
 
 }
